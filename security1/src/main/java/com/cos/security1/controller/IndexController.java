9839a39d70986d78e2bad0e7bf880d1edf311548
daeun han <daeunhan0723@gmail.com>
2024-04-10 23:37:57 +0900

    0.1.1 환경설정

---
+++ security1/src/main/java/com/cos/security1/controller/IndexController.java
@@ -0,0 +1,14 @@
+package com.cos.security1.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class IndexController {
+
+	@GetMapping({ "", "/" })
+	public @ResponseBody String index() {
+		return "인덱스 페이지입니다.";
+	}
+}
